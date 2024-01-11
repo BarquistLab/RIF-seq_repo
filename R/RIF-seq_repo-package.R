912471e1eb7625ac56ea156425ea072e842a851a
Laura Jenniches <laura.jenniches@helmholtz-hiri.de>
2024-01-11 15:06:30 +0100

    Removed to compiled stan model files.

---
+++ R/RIF-seq_repo-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+#' @import tidyverse
+#' @import ggplot2
+#' @import viridis
+#' @import Cairo
+#' @importFrom data.table fread
+#' @importFrom data.table fwrite
+#' @importFrom wCorr weightedCorr
+#' @importFrom MASS fitdistr
+"_PACKAGE"
+
